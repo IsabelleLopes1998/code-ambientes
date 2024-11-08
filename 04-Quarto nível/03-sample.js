5890ec3b871df2ec3c67fbe9165f7f5466f142e2
Isabelle Lopes <isabelle.biomed@gmail.com>
2024-11-08 20:44:05 -0300

    adicionei o try-carch e uma nova função para printar o nome do personagem

--- 04-Quarto nível/03-sample.js
+++ 04-Quarto nível/03-sample.js
@@ -1,7 +1,16 @@
 async function buscarDados() {
-    const dados = "{ name: 'Luke', 'idade': 23 }";
-    const personagem = JSON.parse(dados);
-    console.log(personagem.name);
+    try{
+        const dados = "{\"name\": \"Luke\", \"idade\": 23}";;
+        const personagem = JSON.parse(dados);
+        printNomePersonagem(personagem);
+    }catch(erro){
+        console.erro("JSON Inválido", erro);
+    }
+}
+
+function printNomePersonagem(personagem){
+    const nome = personagem.name;
+    console.log(nome);
 }
 
 buscarDados();
